805f2635a7012dc7f15611d6b8c707098992490e
Raz_Kissos <kissosrazz@gmail.com>
2020-10-04 12:16:22 +0300

    Added makefile for ease of installing modules in the future

--- Data.py
+++ Data.py
@@ -6,7 +6,7 @@ import http.cookiejar
 class Data:
     def __init__(self, url="127.0.0.1",
                  username=None, password=None,
-                 max_pages=-1, port=80, folder=None):
+                 max_pages=-1, port=80, output_folder=None):
         """
         :param url: the address of the target
         :param username: the user's username
@@ -18,7 +18,7 @@ class Data:
         self.address = url
         self.port = port
         self.max_pages = max_pages
-        self.folder = folder
+        self.folder = output_folder
         self.username = username
         self.password = password
         self.pages = list()  # normal pages
---
+++ Makefile
@@ -0,0 +1,4 @@
+MODULE_LIST = mechanize configparser argparse bs4
+
+pip_install: 
+	pip install ${MODULE_LIST}
