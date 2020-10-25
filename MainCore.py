b5e0f1adfd38d62fc9be581d492a18879b10a193
Raz_Kissos <kissosrazz@gmail.com>
2020-10-25 19:36:00 +0200

    prints logo on run

--- MainCore.py
+++ MainCore.py
@@ -4,10 +4,15 @@ import FlagManager
 import PluginManager
 import OutputManager
 import VulnerabilityManager
+import colors
+
+COLOR_MANAGER = colors.Colors()
 
 
 def main():
-    pass
+    # print magestic logo.
+    print(COLOR_MANAGER.BRIGHT_GREEN + "██████╗ ██╗    ██╗███████╗██████╗ \n██╔══██╗██║    ██║██╔════╝██╔══██╗\n██████╔╝██║ █╗ ██║█████╗  ██████╔╝\n██╔═══╝ ██║███╗██║██╔══╝  ██╔══██╗\n██║     ╚███╔███╔╝███████╗██████╔╝\n╚═╝      ╚══╝╚══╝ ╚══════╝╚═════╝ \n" + COLOR_MANAGER.ENDC)
+
     # TODO:
     # data = Flag Manager's return object
     # Plugin Manager actions
