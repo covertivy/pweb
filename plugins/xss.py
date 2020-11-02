cfa420a9008137ede7313a48ceec14a6f34c57ef
Raz_Kissos <kissosrazz@gmail.com>
2020-11-02 16:00:09 +0200

    added fake plugins

---
+++ plugins/xss.py
@@ -0,0 +1,8 @@
+import colors
+
+COLOR_MANAGER = colors.Colors()
+
+
+def check(pages):
+    print(COLOR_MANAGER.PURPLE + COLOR_MANAGER.BOLD + "- XSS check:" + COLOR_MANAGER.ENDC)
+    print(COLOR_MANAGER.PURPLE + "\tnope, nothing yet")
