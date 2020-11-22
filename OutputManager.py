af58e1433b373b5520e9ed891e738f8280c0c52d
Raz Kissos <kissosrazz@gmail.com>
2020-11-22 17:12:11 +0200

    commit before pull to output manager

--- OutputManager.py
+++ OutputManager.py
@@ -3,11 +3,13 @@ import Data
 import os
 from os import path
 
-def print_results(res_dict:dict) -> None:
-    pass
+
+def print_results(res_dict: dict) -> None:
+    for script in res_dict.keys():
+        pass
 
 
-def save_results(res_dict:dict, path:str, clean_save:bool) -> None:
+def save_results(res_dict: dict, path: str, clean_save: bool) -> None:
     """
     A function that saves the results to the output folder.
     Args:
@@ -15,26 +17,30 @@ def save_results(res_dict:dict, path:str, clean_save:bool) -> None:
         path (str): The output folder's path.
         clean_save (bool): A flag to indicate if the save is occuring on a clean folder (True) or an existing one (False).
     """
-    pass
+    for result in res_dict.keys():
+        pass
 
 
-
-def logic(data:Data.Data) -> None:
+def logic(data: Data.Data) -> None:
     res_dict = dict()
-    for result in data.results: # Create a dictionary with each script as the key and it's result list as the value.
+    for (
+        result
+    ) in (
+        data.results
+    ):  # Create a dictionary with each script as the key and it's result list as the value.
         if result.check_name not in res_dict.keys():
             res_dict[result.check_name] = list(result)
         else:
             res_dict[result.check_name].append(result)
-    
+
     # dict might look like : {'xss':[res, res, res], 'bf': [res], 'sqli': []}
-    
-    if data.folder is None: # Check if output folder was given.
+
+    if data.folder is None:  # Check if output folder was given.
         print_results(res_dict)
-    elif not path.exists(data.folder): # Check if given output folder exists
+    elif not path.exists(data.folder):  # Check if given output folder exists
         print(f"Creating Output Folder ({data.folder})...")
-        os.makedirs(data.folder) # Create non existant dir tree.
+        os.makedirs(data.folder)  # Create non existant dir tree.
         save_results(res_dict, data.folder, True)
-    else: # Output folder exists.
+    else:  # Output folder exists.
         print(f"Saving to existing Output Folder ({data.folder})...")
         save_results(res_dict, data.folder, False)
