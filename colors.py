c32536a80837b440df72b236b666a1065db6985a
RazKissos <kissosrazz@gmail.com>
2021-03-14 15:40:13 +0200

    Added function that modifies specific indexes of strings with ANSI modifications.

--- colors.py
+++ colors.py
@@ -14,13 +14,22 @@ LOGO = r"                           __" + "\n" \
 
 def rgb(red: int, green: int, blue: int):
     """
-    Function paints the text
-    @param red: int, 0-255
-    @param green: int, 0-255
-    @param blue: int, 0-255
-    @return: Paint format
+    Function paints the text.
+    @param red (int): Red value of RGB 0-255.
+    @param green (int): Green value of RGB 0-255.
+    @param blue (int): Blue value of RGB 0-255.
+    @returns str: ANSI color value string.
     """
     return "\033[38;2;{};{};{}m".format(red, green, blue)
+
+
+def validate_parameter(param: tuple, strlen: int): 
+    if len(param) != 3:
+        return False
+    type_bool: bool = all((type(param[0]) == int, type(param[1]) == int, type(param[2]) == str))
+    size_bool: bool = all((param[0] >= 0, param[1] > 0, param[0] < param[1], param[1] <= strlen))
+    return type_bool and size_bool
+    
 
 
 class Colors:
@@ -56,21 +65,21 @@ class Colors:
     BOLD_PINK = PINK + BOLD
 
     @staticmethod
-    def rgb(red: int, green: int, blue: int) -> str:
+    def rgb(red: int, green: int, blue: int):
         """
-        Function paints the text
-        @param red: int, 0-255
-        @param green: int, 0-255
-        @param blue: int, 0-255
-        @return: Paint format
+        Function paints the text.
+        @param red (int): Red value of RGB 0-255.
+        @param green (int): Green value of RGB 0-255.
+        @param blue (int): Blue value of RGB 0-255.
+        @returns str: ANSI color value string.
         """
         return rgb(red, green, blue)
 
     @staticmethod
-    def rand_color() -> str:
+    def rand_color():
         """
         Function paints the text in random color
-        @return: Paint format
+        @returns str: Random ANSI color value string.
         """
         return rgb(
             random.choice(range(255)),
@@ -80,10 +89,10 @@ class Colors:
     @staticmethod
     def print_warning(warning: str = "WARNING!", begins_with: str = ""):
         """
-        Function prints a specified warning
-        @param warning: The specified warning
-        @param begins_with: Optional string to start with
-        @return: None
+        Function prints a specified warning.
+        @param warning: The specified warning.
+        @param begins_with: Optional string to start with.
+        @returns None.
         """
         print(
             begins_with
@@ -100,10 +109,10 @@ class Colors:
     @staticmethod
     def print_error(error: str = "ERROR!", begins_with: str = ""):
         """
-        Function prints a specified error
-        @param error: The specified error
-        @param begins_with: Optional string to start with
-        @return: None
+        Function prints a specified error.
+        @param error: The specified error.
+        @param begins_with: Optional string to start with.
+        @returns None.
         """
         print(
             begins_with
@@ -120,10 +129,10 @@ class Colors:
     @staticmethod
     def print_success(success: str = "SUCCESS!", begins_with: str = ""):
         """
-        Function prints a specified success message
-        @param success: The specified message
-        @param begins_with: Optional string to start with
-        @return: None
+        Function prints a given success message.
+        @param success: The specified message.
+        @param begins_with: Optional string to start with.
+        @returns None.
         """
         print(
             begins_with
@@ -136,15 +145,103 @@ class Colors:
             + Colors.BOLD_GREEN
             + success
             + Colors.ENDC)
+    
+    @staticmethod
+    def modify_string(input_str: str, parameters: list):
+        """
+        This function receives a string to modify and the parameters to modify it with.
+        Each parameter is a tuple 3 values long, the first two are start and stop indexes for the wanted modification.
+        ? Important: Please make sure that you send a *LIST* of tuples and not a single tuple.
+        ? Important Note: We strongly advise you to use the included ANSI modifications given in this class.
+        ! PLEASE NOTE: You cannot specify colliding indexes!
+        !   For example: 
+        !       (0, 5) and (2, 10) will not work correctly! if you wish to combine several modifications you can do so by specifying them like so:
+                    > COLOR_MANAGER.modify_string("abcdefg", [(0, 4, COLOR_MANAGER.GREEN + COLOR_MANAGER.BOLD)])
+                    > This will result in a bold green `abcd` and the rest as normal text.
+        ! PLEASE NOTE: the start and stop indexes are not direct indexes but are used like the string indices in python,
+        !   Which means that the stop index is not included in the final string.
+        !   For example:
+        !       in the string "hello, world!", to modify the word `hello` we will use the indexes (0, 5).
+        
+        @param input_str (str): The string to modify with the given parameters.
+        @param parameters (list): The parameters to modify the string with, each parameter is a tuple of:
+        >   (begin_index : int, stop_index : int, ansi_modification : str).
+        @returns str: The modified string with the valid changes made to it, if for some reason there are some missing changes please read the description. 
+        """
+        # Get list of all valid parameters.
+        valid_params = [param for param in parameters if validate_parameter(param, len(input_str))]
+        if len(valid_params) == 0:
+            return input_str
+        
+        # Get lists of all the needed indexes and sort them from last to first.
+        endc_indexes = list(set([param[1] for param in valid_params]))
+        endc_indexes.sort(reverse=True)
+        mod_indexes = list(set([(param[0], param[2]) for param in valid_params]))
+        mod_indexes.sort(key=lambda x: x[0], reverse=True)
+        
+        output_str = Colors.ENDC
+        ansi_to_add = ""
+
+        prev_index = len(input_str)
+        index = 0
+        skip_endc = False
+
+        modi = 0
+        endci = 0
+        while modi < len(valid_params) or endci < len(valid_params):
+            mod_index = None
+            endc_index = None
+            if modi < len(valid_params):
+                mod_index = mod_indexes[modi] # Current modification parameter.
+            if endci < len(valid_params):
+                endc_index = endc_indexes[endci] # Current endc parameter.
+            
+            if mod_index is not None and endc_index is not None:
+                # Check which one should go before the other.
+                if mod_index[0] > endc_index:
+                    index = mod_index[0]
+                    ansi_to_add = mod_index[1]
+                    modi += 1
+                elif mod_index[0] < endc_index:
+                    if not skip_endc:
+                        index = endc_index
+                        ansi_to_add = Colors.ENDC
+                        endci += 1
+                    skip_endc = False
+                else:
+                    # Both need to be used and therefor we must put the endc first so the color will continue.
+                    index = endc_index
+                    ansi_to_add = Colors.ENDC + mod_index[1]
+                    skip_endc = True
+                    modi += 1
+                    endci += 1
+            elif mod_index is not None:
+                index = mod_index[0]
+                ansi_to_add = mod_index[1]
+                modi += 1
+            elif endc_index is not None:
+                index = endc_index
+                ansi_to_add = Colors.ENDC
+                endci += 1
+            
+            # Append the modified text to the output string.
+            output_str = ansi_to_add + input_str[index: prev_index] + output_str
+            prev_index = index
+        
+        if index != 0:
+            # If we did not reach the beginning of the string we need to add it.
+            output_str = input_str[0: prev_index] + output_str
+        
+        return output_str
 
 
 COLOR_MANAGER = Colors()  # Colors instance
 
 
-def startup() -> str:
+def startup():
     """
     Function prints majestic logo.
-    @return: None
+    @returns None.
     """
     logo = ""
     for char in LOGO:
